5f0415e12a7d467d190ee8a20f567c4a74a9b201
TayaneCibely <tayane.cibely@ufape.edu.br>
2024-06-07 23:29:30 -0300

    feat: tela de cadastro de membro

--- app/models/membro.rb
+++ app/models/membro.rb
@@ -1,5 +1,5 @@
 class Membro < ApplicationRecord
-  has_many :tarefas
+  has_many :tarefas, dependent: :destroy
 
   validates :nome, presence: true, length: { minimum: 5 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
